fdebd285a985468cad22bec5304dee3b2a05d1bd
its-namami <164639260+its-namami@users.noreply.github.com>
2025-11-09 08:25:34 +0100

    feat: add my_count and my_none?

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -6,13 +6,14 @@ module Enumerable
     return enum_for(:my_each_with_index) unless block_given?
 
     index = 0
+
     my_each do |element|
       yield element, index
       index += 1
     end
   end
 
-  def my_select
+  def my_select(&block)
     return enum_for(:my_each) unless block_given?
 
     selected = []
@@ -22,6 +23,28 @@ module Enumerable
     end
 
     selected
+  end
+
+  def my_count
+    return size unless block_given?
+
+    count = 0
+
+    my_each do |element|
+      count += 1 if yield element
+    end
+
+    count
+  end
+
+  def my_none?
+    return enum_for(:my_none) unless block_given?
+
+    my_each do |element|
+      return false if yield element
+    end
+
+    true
   end
 end
 
